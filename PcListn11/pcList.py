9c72401f10e1c4d1ca0eb01c88c6131374e28491
tanersekmen <56790802+tanersekmen@users.noreply.github.com>
2021-04-10 20:14:05 +0300

    update pcList.py
    update pcList.py

--- PcListn11/pcList.py
+++ PcListn11/pcList.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def pcList():
-    # site linki
+    # website link
     url_1 = 'https://www.n11.com/bilgisayar/dizustu-bilgisayar'
     page_1 = requests.get(url_1)
     soup_1 = BeautifulSoup(page_1.text, 'html.parser')
@@ -12,9 +12,9 @@ def pcList():
     print('                          Taner Sekmen                      ')
     print('                                                            ')
     print('------------------------------------------------------------')
-    # soup değerinde ürünlerin bulunduğu li yapısını alıyorum.
+    
     tum_degerler = soup_1.findAll('li', {"class": "column"})
-    # İlk sayfadaki değerleri almak için atadığım for döngüsü
+    
     for i in tum_degerler:
         marka_adi = i.h3.text.strip('\n ')
         with open('pcListn11.txt', 'a', encoding='UTF-8') as f:
@@ -32,9 +32,9 @@ def pcListN():
         urlN = 'https://www.n11.com/arama?q=laptop&pg=' + str(n)
         pageN = requests.get(urlN)
         soupN = BeautifulSoup(pageN.text, 'html.parser')
-        # soup değerinde ürünlerin bulunduğu li yapısını alıyorum.
+        
         tum_degerler = soupN.findAll('li', {"class": "column"})
-        # İlk sayfadaki değerleri almak için atadığım for döngüsü
+        
         for i in tum_degerler:
             marka_adi = i.h3.text.strip('\n ')
             with open('pcListn11.txt', 'a', encoding='UTF-8') as f:
